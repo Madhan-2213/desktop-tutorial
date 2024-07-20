e9af932ce49642c1e513424c59c2ee94f862f783
Madhan  S <128589634+Professional-Engineer@users.noreply.github.com>
2024-07-20 10:24:50 +0530

    concatenate.py

---
+++ concatenate.py
@@ -0,0 +1,17 @@
+import numpy
+
+n, m, p = map(int, input().split())
+lst1 = []
+lst2 = []
+
+for i in range(n):
+    lst1.append([*map(int, input().split())])
+arr1 = numpy.array(lst1)
+
+for i in range(m):
+    lst2.append([*map(int, input().split())])
+arr2 = numpy.array(lst2)
+
+print(numpy.concatenate((arr1, arr2), axis=0))
+
+
