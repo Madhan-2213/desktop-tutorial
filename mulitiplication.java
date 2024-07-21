7a3842b4e2e4576dcb7a8c568e8291e9829c1895
Madhan  S <128589634+Professional-Engineer@users.noreply.github.com>
2024-07-21 16:31:36 +0530

    Create mulitiplication.java

---
+++ mulitiplication.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+
+        bufferedReader.close();
+        for(int i = 0; i < 10; i++){
+        int cal = N * (i+1);
+        System.out.printf("%s x %d = %d %n", N, i+1, cal);
+    }
+}
+}
