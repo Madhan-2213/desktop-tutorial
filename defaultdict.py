103280dfabc11743b8187930e6c4e89bb009437e
Madhan  S <128589634+Professional-Engineer@users.noreply.github.com>
2024-07-22 14:44:27 +0530

    defaultdict.py

---
+++ defaultdict.py
@@ -0,0 +1,18 @@
+from collections import defaultdict
+
+df = defaultdict(lambda: -1)
+b = []
+n,m = input().split()
+
+for i in range(1, int(n)+1):
+    vl = input()
+    if vl in df:
+        df[vl] += str(i) + ' '
+    else:
+        df[vl] = str(i) + ' '
+
+for _ in range(int(m)):
+    b.append(input())
+
+for j in b:
+    print(df[j])
