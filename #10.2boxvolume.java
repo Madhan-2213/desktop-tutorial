7ae254199341aaab936922f6c677ef7b1a9de3a3
Madhan  S <128589634+Madhan-2213@users.noreply.github.com>
2025-01-08 20:56:49 +0530

    #10.2boxvolume.java

---
+++ #10.2boxvolume.java
@@ -0,0 +1,15 @@
+public class Main{
+    static void boxvolume(int length,int breadth,int height){
+        int vol = length*breadth*height;
+        System.out.println(vol);
+    }
+
+    public static void main(String[] args) {
+         boxvolume(122,12,12);
+
+
+    }
+}
+
+// output
+//   17568
